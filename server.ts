00958e05bf3920f0ebeeff591320fa0abe310a56
Kai Gartenschläger <mail@kaisnet.de>
2020-05-18 05:06:26 +0200

    Created basic server

---
+++ server.ts
@@ -0,0 +1,12 @@
+import http from "http";
+
+http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "test/html" });
+    response.end(`
+    <html>
+        <body>
+            <h1>Hello World!</h1>
+            You asket for: ${request.url}
+        </body>
+    </html>`);
+}).listen(3000);
