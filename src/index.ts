a758cf6d5fd9f99463a1fa9da93cf5724b34df66
Kai <kai.gartenschlaeger@awin.com>
2020-05-18 09:12:00 +0200

    added reporter

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,4 @@
-class Counter {
-    counter: number = 0;
-    readonly max: number;
+import * as path from "path";
+import * as sh from "./libs/stringHelper";
 
-    constructor(max: number = 10) {
-        this.max = max;
-    }
-
-    increment() {
-        this.counter += 1;
-
-        if (this.counter >= this.max) {
-            console.log("done!");
-        }
-    }
-
-    maxReached(): boolean {
-        return this.counter >= this.max;
-    }
-}
-
-var counter = new Counter(3);
-do {
-    counter.increment();
-} while (!counter.maxReached());
+console.log("test");
---
+++ src/libs/stringHelper.spec.ts
@@ -0,0 +1,9 @@
+import { StringHelper } from "./stringHelper";
+
+describe("stringHelper", function () {
+    const helper = new StringHelper();
+    it("test", function () {
+        let result = helper.test();
+        expect(result).toBe();
+    });
+});
