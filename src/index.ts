ec375081ffdbc05e513c2b89a45598b7b9cbe4cf
Kai <kai.gartenschlaeger@awin.com>
2020-05-26 07:56:41 +0200

    Added base structure for express app

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-import * as path from "path";
-import * as sh from "./libs/stringHelper";
+import * as express from "express";
+const app = express();
+const port = 3000;
 
-console.log("test");
+// define a route handler for the default home page
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+
+// start the Express server
+app.listen(port, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`server started at http://localhost:${port}`);
+});
