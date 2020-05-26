1a65c1302f70a9e6500ff5dcb99f220269810d83
Kai <kai.gartenschlaeger@awin.com>
2020-05-26 10:06:44 +0200

    Code refactorings

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.SERVER_PORT;
+const PORT = Environment.getPort();
 
 app.use((request, response, next) => {
     next();
